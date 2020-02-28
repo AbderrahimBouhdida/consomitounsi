aa611fa0af09a1928804bdd450733965f5c60c72
Imousrf <abderrahim.bouhdida@gmail.com>
2020-02-28 16:37:40 +0100

    heheheh

--- consomitounsi/consomitounsi-web/src/main/java/consomitounsi/ressources/JAXRSConfiguration.java
+++
@@ -1,8 +0,0 @@
-package consomitounsi.ressources;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JAXRSConfiguration extends Application {
-
-}
