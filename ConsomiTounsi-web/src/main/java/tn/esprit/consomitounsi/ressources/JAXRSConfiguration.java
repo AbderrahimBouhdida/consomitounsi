440de0cfd048794ae42a18a73d8c82596035b600
Imousrf <abderrahim.bouhdida@gmail.com>
2020-03-01 01:42:05 +0100

    clean

--- ConsomiTounsi-web/src/main/java/tn/esprit/consomitounsi/ressources/JAXRSConfiguration.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.consomitounsi.ressources;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JAXRSConfiguration extends Application {
-
-}
