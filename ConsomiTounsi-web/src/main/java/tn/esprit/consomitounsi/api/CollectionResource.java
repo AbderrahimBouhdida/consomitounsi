d60c234b497825fb6787545e4fad60b5b97b224e
Taib <taib.adhibi@esprit.tn>
2020-03-27 17:16:25 +0100

    first gitkrakentest

--- ConsomiTounsi-web/src/main/java/tn/esprit/consomitounsi/api/CollectionResource.java
+++ ConsomiTounsi-web/src/main/java/tn/esprit/consomitounsi/api/CollectionResource.java
@@ -8,7 +8,7 @@ import javax.ws.rs.Consumes;
 //import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-//import javax.ws.rs.PUT;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -39,7 +39,7 @@ public class CollectionResource {
         return Response.ok(cols.findAllCollection()).build();
     }
     
-    @POST
+    @PUT
     @Path("/edit")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
